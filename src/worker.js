29852c3fee62a3a68ad8073c3402d79645bfae6e
Takashi Kanemoto <ttskch@gmail.com>
2020-05-21 11:58:01 +0900

    test

--- src/worker.js
+++ src/worker.js
@@ -1,1 +1,11 @@
 console.log('worker started')
+
+setInterval(() => {
+  console.log('worker running...')
+}, 1000 * 60)
+
+process.stdin.resume(); // prevent to exit
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received')
+})
